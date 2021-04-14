570570e117a3fc2835946f79c786dccb734ba5a9
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-04-14 20:19:51 -0300

    Update Lista.c

---
+++ Fonte/Lista.c
@@ -0,0 +1,22 @@
+#include "Lista.h"
+
+int TamanhoLista(Lista *L)
+{
+}
+
+void NovaFila(Lista *L);
+void Enfilerar(Lista *L);
+
+void InserirFinal(Lista *L);
+void InserirInicio(Lista *L);
+
+
+void Pesquisar(Lista *L, );
+
+void RemoverFinal(Lista *L);
+void RemoverInicio(Lista *L);
+
+void RemoverEspecifico();
+void AlterarProduto(Lista *L, int Codigo);
+
+void ImprimirLista(Lista *L);
