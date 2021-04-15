e5f1195837985115156c02b93c834d8f8b34eae6
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-04-15 20:52:10 -0300

    Update Lista.h

--- Fonte/Testes/Lista.h
+++ Fonte/Testes/Lista.h
@@ -38,7 +38,7 @@ int TamanhoLista(Lista *L)
 	return L->tam;
 }
 
-void NovaFila(Lista *L)
+void NovaLista(Lista *L)
 {
 	Celula *nova = NovaCelula();
 
@@ -47,16 +47,58 @@ void NovaFila(Lista *L)
 	L->tam = 0;
 }
 
-void InserirFinal(Lista *L);
+void InserirFinal(Lista *L, Produto P)
+{
+	Celula *tmp = NovaCelula();
+	tmp->dado = P;
+
+	L->fim->prox = tmp;
+	L->fim = tmp;
+	L->tam++;
+}
+
 void InserirInicio(Lista *L);
 
 
 //void Pesquisar(Lista *L, );
+void RemoverEspecifico(Lista *L, int Codigo)
+{
+	Celula *Anterior = L->inicio;
+	Celula *Auxiliar = L->inicio->prox;
+	
+	while(Auxiliar!=NULL && Auxiliar->dado.codigo != Codigo)
+	{
+		Anterior = Anterior->prox;
+		Auxiliar = Anterior->prox;
+	}
+	
+	if(Auxiliar!=NULL)
+	{
+		Anterior->prox = Auxiliar->prox;
+		free(Auxiliar);	
+		Auxiliar = NULL;
+		
+		if(Anterior->prox == NULL)
+		{
+			L->fim = Anterior;
+		}
+		
+		L->tam--;
+	}
+}
 
-void RemoverFinal(Lista *L);
-void RemoverInicio(Lista *L);
+void RemoverInicio(Lista *L)
+{
+	RemoverEspecifico(L,0);
+}
 
-void RemoverEspecifico();
+void RemoverFinal(Lista *L)
+{
+	RemoverEspecifico(L,L->tam-1);
+}
+
+
+
 void AlterarProduto(Lista *L, int Codigo);
 
 void ImprimirCelula(Celula C)
@@ -65,6 +107,38 @@ void ImprimirCelula(Celula C)
 	printf("Descrição → %s\n",C.dado.descricao);
 	printf("Valor → %.2f\n",C.dado.valor);
 	printf("Quantidade → %d\n",C.dado.quantidade);
+}
+
+void ImprimirProduto(Produto P)
+{
+	printf("| %d      | %s                 | %.2f | %d         |\n",P.codigo,P.descricao,P.valor,P.quantidade);
+}
+
+void ImprimirLista(Lista *L)
+{
+	printf("+--------+---------------------------+-------+------------+\n");
+	printf("| Código |         Descrição         | Valor | Quantidade |\n");
+	printf("+--------+---------------------------+-------+------------*\n");
+	
+	Celula *tmp = L->inicio->prox;
+	while(tmp != NULL)
+	{
+		ImprimirProduto(tmp->dado);
+		printf("-----------------------------------------------------------\n");
+		tmp = tmp->prox;
+	}
+}
+
+
+
+void Enfilerar(Lista *L, Produto dado)
+{
+  Celula *nova = NovaCelula();
+  nova->dado = dado;
+
+  L->fim->prox = nova;
+  L->fim = nova;
+  L->tam++;
 }
 
 //Demais funcionalidades do sistema
