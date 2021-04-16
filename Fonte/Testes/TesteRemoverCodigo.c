eb82f17c6e5c7446bbd9bce6b522b6f71f205070
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-04-15 21:10:02 -0300

    Create TesteRemoverCodigo.c

---
+++ Fonte/Testes/TesteRemoverCodigo.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <stdio.h>
+#include "Lista.h"
+
+int main() 
+{
+
+  Lista Estoque;
+  NovaLista(&Estoque);
+
+  for(int i=0; i<5; i++)
+  {
+    Produto p;
+    p.codigo = i+1;
+    sprintf(p.descricao, "Produto %d", i+1);
+    p.valor = rand()%100;
+    p.quantidade = rand()%50;
+
+    InserirFinal(&Estoque, p);   
+
+  }
+
+ 
+  ImprimirLista(&Estoque);
+  RemoverEspecifico(&Estoque,3);
+
+  printf("\nApós excluir:\n");
+  ImprimirLista(&Estoque);
+  
+  return 0;
+}
