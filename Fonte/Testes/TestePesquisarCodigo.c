c7e4aeafcb0bbce1c828993664755473e12bf6b6
Pedro Igor <55034603+ORZAYAZ@users.noreply.github.com>
2021-04-16 11:00:18 -0300

    Create TestePesquisarCodigo.c

---
+++ Fonte/Testes/TestePesquisarCodigo.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdio.h>
+#include "Lista.h"
+
+int main() 
+{
+
+  Lista Estoque;
+  NovaLista(&Estoque);
+
+  for(int i=0; i<5; i++)
+  {
+    Produto p;
+    p.codigo = rand()%10;
+    sprintf(p.descricao, "Produto %d", rand()%10);
+    p.valor = rand()%100;
+    p.quantidade = rand()%50;
+
+    InserirFinal(&Estoque, p);   
+  }
+
+  ImprimirLista(&Estoque);
+  printf("\nTamanho da Lista → %d\n",TamanhoLista(&Estoque));
+  
+  Pesquisar(&Estoque,8);
+  
+  return 0;
+}
